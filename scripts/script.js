a0b6cc2517c872ea01af56eae050de930d35936e
Unknown <trixter5@yandex.ru>
2018-02-20 18:57:34 +0300

    almost all css

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,14 @@
 
 $(function () {
-
+	$('.section--block-8__slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		dots: false,
+		centerMode: true,
+		initialSlide: 1,
+		variableWidth: true
+	});
  
 
 });
